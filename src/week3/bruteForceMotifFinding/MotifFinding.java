a623599c0b8c9e95a1d2b57667b073634325878b
callieliza <calidesigns42@gmail.com>
2017-01-25 13:37:36 -0500

    working on week 3 challenges

---
+++ src/week3/bruteForceMotifFinding/MotifFinding.java
@@ -0,0 +1,23 @@
+package week3.bruteForceMotifFinding;
+
+/**
+ * Created by chewy on 1/25/17.
+ */
+
+/*****
+      MotifEnumeration(Dna, k, d)
+        Patterns ← an empty set
+        for each k-mer Pattern in the first string in Dna
+        for each k-mer Pattern’ differing from Pattern by at most d mismatches
+        if Pattern' appears in each string from Dna with at most d mismatches
+        add Pattern' to Patterns
+        remove duplicates from Patterns
+        return Patterns
+*****/
+
+
+public class MotifFinding {
+    public void MofitEnumeration() {
+
+    }
+}
